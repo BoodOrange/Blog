055fc9f4c41d2c0e73129275f5f6ba64254c5f23
zluzunov <zluzunov@gmail.com>
2017-05-20 16:28:05 +0300

    Added Chrome driver. Tests work.

--- Blog.UI.Tests/BrowserHost.cs
+++ Blog.UI.Tests/BrowserHost.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,13 +11,12 @@ namespace Blog.UI.Tests
     public static class BrowserHost
     {
         public static readonly SelenoHost Instance = new SelenoHost();
-        private static readonly string RootUrl = "http://localhost:60634/Article/List";
+        public static readonly string RootUrl = @"http://localhost:60634/Article/List";
 
         static BrowserHost()
         {
-            Instance.Run("Blog", 60639);
-            // For Chrome: Instance.Run("Blog", 60634, w => w.WithRemoteWebDriver(BrowserFactory.Chrome));
-            // or Instance.Run("Blog", 60634, w => w.WithRemoteWebDriver(() => new ChromeDriver()));
+            //Instance.Run("Blog", 60634); // Runing for FireFox
+            Instance.Run("Blog", 60634, w => w.WithRemoteWebDriver(() => new ChromeDriver())); // Runing for chrome 
 
         }
     }
